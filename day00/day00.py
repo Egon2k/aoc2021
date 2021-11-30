0c58ac761fcf926f0a48a8d96e7d3a462fbf95ab
Rene Trampenau <rtrampenau@gmail.com>
2021-11-30 20:38:06 +0100

    updated template

--- day00/day00.py
+++ day00/day00.py
@@ -1,6 +1,15 @@
+def part1():
+    pass
+
+def part2():
+    pass
+
 if __name__ == "__main__":
     data = []
 
     with open('data.txt') as f:
         for line in f:
             data.append(line.strip())
+
+    print(part1(data))
+    print(part2(data))
