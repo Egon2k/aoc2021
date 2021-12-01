4c372086b90f5867962b568ab047650ea84cf80b
Rene Trampenau <rtrampenau@gmail.com>
2021-12-01 05:56:57 +0100

    preparation for day01

--- day01/day01.py
+++ day01/day01.py
@@ -7,7 +7,7 @@ def part2(data):
 if __name__ == "__main__":
     data = []
 
-    with open('day00/data.txt') as f:
+    with open('day01/data.txt') as f:
         for line in f:
             data.append(line.strip())
 
