c1a0163b495c765835269e98bb2825c031a8c04d
Rene Trampenau <rtrampenau@gmail.com>
2021-12-05 09:38:44 +0100

    solved day05 part1

---
+++ day05/day05.py
@@ -0,0 +1,63 @@
+'''''''''''''''''''''
+Ey, nicht spicken :P 
+'''''''''''''''''''''
+def count(grid, threshold):
+    count = 0
+    for line in grid:
+        for nmbr in line:
+            if nmbr > threshold:
+                count += 1
+    return count
+
+def sortCoords(instr):
+    i = instr[:]
+    if i[0] > i[2]:
+        temp = i[2]
+        i[2] = i[0]
+        i[0] = temp
+    
+    if i[1] > i[3]:
+        temp = i[3]
+        i[3] = i[1]
+        i[1] = temp
+    return i
+
+def part1(instrs):
+    dim = 1000
+    grid = [[0]*dim for i in range(dim)] # create grid with dim x dim
+    
+    for instr in instrs:
+        if instr[0] == instr[2]: # x coords are the same
+            i = sortCoords(instr)
+            for y in range(i[1],i[3]+1):
+                grid[y][i[0]] += 1
+        elif instr[1] == instr[3]: # y coords are the same
+            i = sortCoords(instr)
+            for x in range(i[0],i[2]+1):
+                grid[i[1]][x] += 1
+        else:
+            # ignore for now (and we all know it will be important in part2)
+            pass
+    return count(grid, 1)
+
+def part2(instrs):
+    pass
+
+def prepareData(data):
+    instr = list()
+    for line in data:
+        instr.append([int(i) for i in line.replace(' -> ', ',').split(',')])
+    return instr
+
+if __name__ == "__main__":
+    data = []
+
+    with open('day05/data.txt') as f:
+        for line in f:
+            data.append(line.strip())
+      
+    # prepare data for processing
+    instrs = prepareData(data)
+        
+    print(part1(instrs))
+    print(part2(instrs))
