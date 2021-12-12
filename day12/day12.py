48d3c255b4345a5afe1e1f925956f7a5c66e1d80
Rene Trampenau <rtrampenau@gmail.com>
2021-12-12 22:40:05 +0100

    solved day12

--- day12/day12.py
+++ day12/day12.py
@@ -25,9 +25,33 @@ def part1(caves):
                 queue.append((dest, newVisitedSmalls))
     return sum
 
+def part2(caves):
+    currCaveInit = 'start'
+    visitedSmallsInit = set()
+    visitedSmallsInit.add('start')
+    queue = [(currCaveInit, visitedSmallsInit, "")]
+    sum = 0
 
-def part2(data):
-    pass
+    while(queue):
+        currCave, visitedSmalls, doubleSmall = queue.pop(0)
+
+        if currCave == 'end':
+            sum += 1
+            continue
+
+        for dest in caves[currCave]:
+            if dest not in visitedSmalls:
+                newVisitedSmalls = set(visitedSmalls)
+                if dest.lower() == dest:
+                    newVisitedSmalls.add(dest)
+                # passthrough the cave that was visited twice
+                queue.append((dest, newVisitedSmalls, doubleSmall))
+            else:
+                if doubleSmall == "" and dest != 'start' and dest != 'end':
+                    # save the cave that was visited twice so no other small 
+                    # cave is visited twice
+                    queue.append((dest, visitedSmalls, dest))
+    return sum
 
 if __name__ == '__main__':
     data = []
