5a9178537fb3c8c8cef31111811199c33c044890
Rene Trampenau <rtrampenau@gmail.com>
2021-12-18 09:28:08 +0100

    wip

--- day17/day17.py
+++ day17/day17.py
@@ -1,18 +1,16 @@
 '''
 Gewalt ist auch eine Lösung!
 '''
-def shoot(x_start, y_start, x_target, y_target):
+def shoot(x_start, y_start):
     pos = list((0, 0))
     y_max = 0
     x = x_start
     y = y_start
-    while (not(x > max(x_target) or y < min(y_target))):
+    while (not(x > 161 or y < -154)):
         pos[0] += x
         if x > 0:
             x -= 1
-        elif x == 0:
-            pass
-        else:
+        elif x < 0:
             x += 1
         pos[1] += y
         y -= 1
@@ -20,17 +18,17 @@ def shoot(x_start, y_start, x_target, y_target):
         if pos[1] > y_max:
             y_max = pos[1]
 
-        if pos[0] in x_target and pos[1] in y_target:
+        if 111 <= pos[0] <= 161 and -154 <= pos[1] <= -101:
             return y_max, True
     
     return 0, False
         
-def part12(x_target, y_target):
+def part12():
     y_hits = list()
     
-    for x in range(161):
-        for y in range(-154,1000):
-            y_max, hit = shoot(x, y, x_target, y_target)
+    for x in range(165):
+        for y in range(-155, 1000):
+            y_max, hit = shoot(x, y)
             if hit:
                 y_hits.append(y_max)
                 print(max(y_hits), len(y_hits))
@@ -44,7 +42,4 @@ if __name__ == '__main__':
         for line in f:
             data.append(line.strip())
 
-    x_target = range( 111, 161)
-    y_target = range(-154,-101)    
-
-    print(f'final = {part12(x_target, y_target)}')
+    print(f'final = {part12()}')
