4534185b8769a537c5ce57698b898273888ec39e
Rene Trampenau <rtrampenau@gmail.com>
2021-12-18 09:34:40 +0100

    solved day17

--- day17/day17.py
+++ day17/day17.py
@@ -1,12 +1,21 @@
 '''
 Gewalt ist auch eine Lösung!
 '''
+
+# puzzle data
+# target area: x=111..161, y=-154..-101
+
+X_LOWER = 111
+X_UPPER = 161
+Y_LOWER = -154
+Y_UPPER = -101
+
 def shoot(x_start, y_start):
     pos = list((0, 0))
     y_max = 0
     x = x_start
     y = y_start
-    while (not(x > 161 or y < -154)):
+    while (not(x > X_UPPER or y < Y_LOWER)):
         pos[0] += x
         if x > 0:
             x -= 1
@@ -18,7 +27,7 @@ def shoot(x_start, y_start):
         if pos[1] > y_max:
             y_max = pos[1]
 
-        if 111 <= pos[0] <= 161 and -154 <= pos[1] <= -101:
+        if X_LOWER <= pos[0] <= X_UPPER and Y_LOWER <= pos[1] <= Y_UPPER:
             return y_max, True
     
     return 0, False
@@ -26,8 +35,8 @@ def shoot(x_start, y_start):
 def part12():
     y_hits = list()
     
-    for x in range(165):
-        for y in range(-155, 1000):
+    for x in range(X_UPPER + 4):
+        for y in range(Y_LOWER - 1, 1000):
             y_max, hit = shoot(x, y)
             if hit:
                 y_hits.append(y_max)
@@ -36,10 +45,4 @@ def part12():
     return max(y_hits), len(y_hits)
 
 if __name__ == '__main__':
-    data = []
-
-    with open('day17/data.txt') as f:
-        for line in f:
-            data.append(line.strip())
-
     print(f'final = {part12()}')
