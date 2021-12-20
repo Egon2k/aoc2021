9a1ba7fa444bb781608c9d1be5945e7c19ea77dc
Rene Trampenau <rtrampenau@gmail.com>
2021-12-20 15:35:11 +0100

    solved day20 part1

--- day20/day20.py
+++ day20/day20.py
@@ -1,18 +1,18 @@
-def get_pixel(row, col, input):
+def get_pixel(row, col, input, switch):
     if row < 0 or col < 0 or row >= len(input) or col >= len(input[0]):
-        return 0
+        return switch
     else:
         return input[row][col]
-        
-def get_neighbors(row, col, input):
+
+def get_neighbors(row, col, input, switch):
     out = [[0,0,0] for i in range(3)]
 
     for r, rr in enumerate(range(row - 1,row + 2)):
         for c, cc in enumerate(range(col - 1,col + 2)):
-            out[r][c] = get_pixel(rr, cc, input)
+            out[r][c] = get_pixel(rr, cc, input, switch)
     return out
 
-def solve(algo, input):
+def solve(algo, input, switch):
     output_rows = len(input) + 2
     output_cols = len(input[0]) + 2
 
@@ -20,21 +20,32 @@ def solve(algo, input):
 
     for row in range(output_rows):
         for col in range(output_cols):
-            idx = get_neighbors(row - 1, col - 1, input)
+            idx = get_neighbors(row - 1, col - 1, input, switch)
             idx_str = ""
             for x in range(len(idx)):
                 for y in range(len(idx[0])):
                     idx_str += str(idx[x][y])
-            
+
             output[row][col] = algo[int(idx_str,2)]
-            
+
     return output
 
-    
+def print_picture(input):
+    for row in input:
+        for col in row:
+            if col == '0':
+                print('.', end='')
+            else:
+                print('#', end='')
+        print('')
+    print('')
+
 def part1(algo, input):
     output = input
-    for _ in range(2):
-        output = solve(algo, output)
+    print_picture(output)
+    for x in range(2):
+        output = solve(algo, output, x % 2)
+        print_picture(output)
 
     ans = 0
     for row in output:
@@ -53,7 +64,6 @@ if __name__ == '__main__':
         for i, line in enumerate(f):
             if i == 0:
                 algo = list(line.strip().replace('.', '0').replace('#', '1'))
-                
             elif i > 1:
                 input.append(list(line.strip().replace('.', '0').replace('#', '1')))
 
